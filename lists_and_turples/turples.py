6987e279c55f231edc4927ecdfa896d1e182c64d
Brian Kimathi <kimathibrian71@gmail.com>
2024-01-15 21:49:07 +0300

    commit

---
+++ lists_and_turples/turples.py
@@ -0,0 +1,21 @@
+tup = tuple()
+print(tup)
+
+tup1 = (2)
+print(type(tup1))
+
+tup2 = (2,)
+print(type(tup2))
+
+name = "briankimathi"
+tup3 = tuple(name)
+print(tup3)
+
+list1 = [1,2,3,4]
+tup4 = tuple(list1)
+print(tup4)
+
+
+print(tup3.count("i"))
+
+print(tup3.index("i"))
